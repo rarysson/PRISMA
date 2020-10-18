6c39f10b6253200d4179e4c1124b826f06f57daa
Rarysson Guilherme <r_guilherme12@hotmail.com>
2020-10-18 19:24:02 -0300

    Adicionado testes ao componente JointPaper

---
+++ tests/unit/paper.spec.js
@@ -0,0 +1,79 @@
+import { shallowMount } from "@vue/test-utils";
+import JointPaper from "@/components/Joint/JointPaper";
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = shallowMount(JointPaper);
+});
+
+afterEach(() => {
+    wrapper.destroy();
+});
+
+describe("Componente JointPaper", () => {
+    test("Está instanciado", () => {
+        expect(wrapper.exists()).toBeTruthy();
+    });
+
+    test("Paper data é não nulo após componente instanciado", () => {
+        expect(wrapper.vm.paper).not.toBeNull();
+    });
+
+    test("Emite evento mounted", () => {
+        expect(wrapper.emitted().mounted.length).toBe(1);
+    });
+
+    test("Evento mounted retorna um objeto graph", () => {
+        expect(typeof wrapper.emitted().mounted[0][0]).toBe("object");
+    });
+
+    test("Paper é destruído", () => {
+        wrapper.destroy();
+
+        expect(wrapper.exists()).toBeFalsy();
+    });
+
+    test("Emite evento blank-click", () => {
+        wrapper.vm.$emit("blank-click", { x: 0, y: 0 });
+        const { x, y } = wrapper.emitted()["blank-click"][0][0];
+
+        expect(wrapper.emitted()["blank-click"].length).toBe(1);
+        expect(typeof wrapper.emitted()["blank-click"][0]).toBe("object");
+        expect(x).toBe(0);
+        expect(y).toBe(0);
+    });
+
+    test("Emite evento element-click", () => {
+        wrapper.vm.$emit("element-click", {
+            id: "d61d6f96-b520-4e52-9436-14dcdc6a61ea",
+            type: "place"
+        });
+        const { id, type } = wrapper.emitted()["element-click"][0][0];
+
+        expect(wrapper.emitted()["element-click"].length).toBe(1);
+        expect(typeof wrapper.emitted()["element-click"][0]).toBe("object");
+        expect(id).toBe("d61d6f96-b520-4e52-9436-14dcdc6a61ea");
+        expect(type).toBe("place");
+    });
+
+    test("Emite evento element-contextmenu", () => {
+        wrapper.vm.$emit("element-contextmenu", {
+            id: "d61d6f96-b520-4e52-9436-14dcdc6a61ea",
+            type: "transition",
+            position: { x: 50, y: 50 }
+        });
+        const { id, type, position } = wrapper.emitted()[
+            "element-contextmenu"
+        ][0][0];
+
+        expect(wrapper.emitted()["element-contextmenu"].length).toBe(1);
+        expect(typeof wrapper.emitted()["element-contextmenu"][0]).toBe(
+            "object"
+        );
+        expect(id).toBe("d61d6f96-b520-4e52-9436-14dcdc6a61ea");
+        expect(type).toBe("transition");
+        expect(position.x).toBe(50);
+        expect(position.y).toBe(50);
+    });
+});
