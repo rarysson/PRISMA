ffc9b4c820b9ed23f73151b0d96b3e64733b02b9
Rarysson Guilherme <r_guilherme12@hotmail.com>
2020-10-31 10:48:41 -0300

    Estilizado componente ModalSimulation

--- src/components/Widgets/Modal/ModalSimulation.vue
+++ src/components/Widgets/Modal/ModalSimulation.vue
@@ -2,19 +2,25 @@
   <modal v-model="open">
     <form @submit.prevent="submit">
       <label for="steps">Insira o número de passos</label>
+      <br />
       <input type="number" id="steps" min="1" v-model.number="steps" required />
+      <br />
 
       <input type="checkbox" id="skip" v-model="skip_animation" />
       <label for="skip">Desativar animação durante o processo</label>
 
-      <button type="button" @click="open = false">cancelar</button>
-      <button type="submit">confirmar</button>
+      <div class="btns-container">
+        <btn-close @click="close_modal">Cancelar</btn-close>
+        <btn-confirm>Confirmar</btn-confirm>
+      </div>
     </form>
   </modal>
 </template>
 
 <script>
 import Modal from "./Modal";
+import BtnClose from "@/components/Widgets/Btns/BtnClose";
+import BtnConfirm from "@/components/Widgets/Btns/BtnConfirm";
 
 export default {
   name: "ModalSimulation",
@@ -27,7 +33,9 @@ export default {
   },
 
   components: {
-    Modal
+    Modal,
+    BtnClose,
+    BtnConfirm
   },
 
   data() {
@@ -54,6 +62,12 @@ export default {
         steps: this.steps,
         skip_animation: this.skip_animation
       });
+    },
+
+    close_modal() {
+      this.open = false;
+      this.steps = 1;
+      this.skip_animation = false;
     }
   }
 };
@@ -61,9 +75,36 @@ export default {
 
 <style scoped>
 /deep/ .modal {
-  width: 550px;
-  height: 350px;
+  width: 400px;
+  height: 250px;
   background-color: whitesmoke;
-  padding: 3%;
+  padding: 1.5% 1%;
+}
+
+form {
+  position: relative;
+  height: 100%;
+  padding-top: 25px;
+  text-align: center;
+}
+
+#steps {
+  width: 20%;
+  margin: 10px 0 25px;
+  border: none;
+  text-align: center;
+}
+
+#skip {
+  margin-right: 10px;
+}
+
+.btns-container {
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
